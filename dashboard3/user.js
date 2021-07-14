3001dbad83ccd439005f6c603a2ba1c33522bb93
quytt <tranthienquy98@gmail.com>
2021-07-14 22:58:28 +0700

    quytt

---
+++ dashboard3/user.js
@@ -0,0 +1,22 @@
+function btnSelectType(myTag, type) {
+    let tag = $(".btn_type_select");
+    let tagUser = $(".userRecord");
+    tag.removeAttr("class");
+    tag.attr("class", "btn_type_select")
+    let newMyTag = $(myTag);
+    newMyTag.attr("class", "btn_type_select btn_type_selected")
+    switch (type) {
+        case 1:
+            tagUser.css("display", "block");
+            break;
+        case 2:
+            tagUser.each((index, val)=>{
+                if(val.attr("data-user")==="quan"){
+                    val.css("display","block")
+                }else{
+                    val.css("display","none")
+                }
+            })
+            break;
+    }
+}
